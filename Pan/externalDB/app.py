0c8937acc641584e16105ff8c2700d30dc148467
Pannic <pangl2000@gmail.com>
2023-12-29 16:19:20 +0200

    first commit

--- Pan/externalDB/app.py
+++ Pan/externalDB/app.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import json
 import connexion
 from flask_cors import CORS
@@ -19,16 +20,96 @@ def getEntityRoute():
 
     # Get the MongoDB collection
     collection = mongo.db['your_collection']
-
+    data = []
     try:
         # Find the entity by custom_id
-        existing_entity = collection.find_one({'id': entityId})
+        for existing_entity in collection.find({'id': entityId}):
+            existing_entity.pop('_id',None)
+            data.append(existing_entity)
 
-        if not existing_entity:
+        if len(list(collection.find({'id': entityId}))) == 0:
             return jsonify({'message': f'Entity with ID {entityId} not found'}), 404
-        
-        existing_entity.pop('_id',None)
-        return jsonify({'message': f'Entity with ID {entityId} received', 'data': existing_entity})
+
+        return jsonify({'message': f'Entity with ID {entityId} received', 'data': data})
+    
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return jsonify({'message': 'Error occurred during get'}), 500
+    
+
+@app.route('/get_entities_by_time', methods=['GET'])
+def getEntitiesByTimeRoute():
+    entityId = request.args.get('id')
+    initYear = int(request.args.get('Year from'))
+    initMonth = int(request.args.get('Month from'))
+    initDay = int(request.args.get('Day from'))
+    initHour = int(request.args.get('Hour from'))
+    initMinute = int(request.args.get('Minute from'))
+    initSecond = int(request.args.get('Second from'))
+
+    endYear = int(request.args.get('Year to'))
+    endMonth = int(request.args.get('Month to'))
+    endDay = int(request.args.get('Day to'))
+    endHour = int(request.args.get('Hour to'))
+    endMinute = int(request.args.get('Minute to'))
+    endSecond = int(request.args.get('Second to'))
+
+    # Get the MongoDB collection
+    collection = mongo.db['your_collection']
+    data = []
+    try:
+        # Find the entity by custom_id, date and time
+        for existing_entity in collection.find({
+                                                "dateObserved":
+                                                    {
+                                                    "type": "datetime",
+                                                    "value": 
+                                                    {
+                                                        "$gte": dt.datetime(initYear, initMonth, initDay, 
+                                                                            initHour, initMinute, initSecond),
+                                                        "$lt": dt.datetime(endYear, endMonth, endDay, 
+                                                                            endHour, endMinute, endSecond)
+                                                    }
+                                                    },
+                                                "id": entityId
+                                                }):
+            existing_entity.pop('_id',None)
+            data.append(existing_entity)
+        print(list(collection.find({
+                                    "dateObserved": 
+                                        {
+                                        "type": "datetime",
+                                        "value": 
+                                            {
+                                            "$gte": dt.datetime(initYear, initMonth, initDay, 
+                                                                initHour, initMinute, initSecond),
+                                            "$lt": dt.datetime(endYear, endMonth, endDay, 
+                                                                endHour, endMinute, endSecond)
+                                            }
+                                        },
+                                    "id": entityId
+                                    }
+                                    )))
+        if len(list(collection.find({
+                                    "dateObserved": 
+                                        {
+                                        "type": "datetime",
+                                        "value": 
+                                            {
+                                            "$gte": dt.datetime(initYear, initMonth, initDay, 
+                                                                initHour, initMinute, initSecond),
+                                            "$lt": dt.datetime(endYear, endMonth, endDay, 
+                                                                endHour, endMinute, endSecond)
+                                            }
+                                        },
+                                    "id": entityId
+                                    }
+                                    ))) == 0:
+            print(dt.datetime(initYear,initMonth,initDay,initHour,initMinute,initSecond))
+            print(dt.datetime(endYear,endMonth,endDay,endHour,endMinute,endSecond))
+            return jsonify({'message': f'Entity with ID {entityId} not found'}), 404
+
+        return jsonify({'message': f'Entity with ID {entityId} received', 'data': data})
     
     except Exception as e:
         print(f"Error: {str(e)}")
