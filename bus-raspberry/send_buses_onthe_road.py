98b536f44b801af8b01893815279b338391d3a9d
n0tw <evgenia.123@hotmail.com>
2024-01-17 14:05:46 +0200

    send buses on the road

---
+++ bus-raspberry/send_buses_onthe_road.py
@@ -0,0 +1,25 @@
+import subprocess, time
+
+def run_busai(script_path, arguments):
+    command = ['python', script_path] + arguments
+
+    try:
+        subprocess.Popen(command)
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+# Example usage:
+busai_script = 'busai.py'
+ids = []
+i=1
+
+while i <6:
+    ids.append(['urn:ngsi-ld:Vehicle:vehicle:Bus:'+str(i), 'urn:ngsi-ld:CrowdFlowObserved:Bus:'+str(i)])
+    run_busai(busai_script, ids[-1])
+    i+=1
+    time.sleep(240)
+
+
+
