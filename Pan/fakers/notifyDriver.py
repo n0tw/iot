c7ee37ed24c5074f281a3bfda466870d501ef31c
Pannic <pangl2000@gmail.com>
2024-01-26 18:04:12 +0200

    notify

--- Pan/fakers/notifyDriver.py
+++ Pan/fakers/notifyDriver.py
@@ -1,31 +1,51 @@
 from flask import Flask, request
 import requests
+import schedule
+import time
 
 app = Flask(__name__)
 
 # Replace 'YOUR TELEGRAM BOT TOKEN' with your actual Telegram Bot token
 TOKEN = '6971283115:AAGHpKPd5hXSbDf6afYORNbIK7oohLrjwiI'
 TELEGRAM_API_BASE_URL = f"https://api.telegram.org/bot{TOKEN}/"
-# The following link was assigned from ngrok after installing it and running (ngrok http 3000) in terminal.
-# This way, the port 3000 becomes exposed to the public.
-# NGROK_PUBLIC_URL = " https://07e6-2a02-587-796f-2100-1941-bb00-334b-7dc.ngrok-free.app/"
 
-chat_ids = []
-url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
-data = requests.get(url).json()
-for doc in data:
-    if doc['message']['chat']['id'] not in chat_ids:
-         chat_ids.append(data['message']['chat']['id'])
-        
-# @app.route('/webhook', methods=['POST'])
-# def webhook():
-#     global chat_ids
-#     data = request.get_json()
-#     # Process the received update as needed
-#     print("Received update:", data)
-#     if data['message']['chat']['id'] not in chat_ids:
-#         chat_ids.append(data['message']['chat']['id'])
-#     return '', 200
+def read_words_from_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            words = file.read().splitlines()
+        return words
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+        return []
+
+def save_words_to_file(file_path, words):
+    with open(file_path, 'w') as file:
+        for word in words:
+            file.write(word + '\n')
+
+def collect_chat_ids():
+    file_path = 'chatIds.txt'  # Change this to the path of your txt file
+
+    # Read words from the file
+    chat_ids = read_words_from_file(file_path)
+    print("Current chat IDs in the list:")
+    print(chat_ids)
+
+    url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
+    data = requests.get(url).json()
+    for doc in data:
+        if doc['message']['chat']['id'] not in chat_ids:
+            chat_ids.append(doc['message']['chat']['id'])
+
+    # Save the updated list back to the file
+    save_words_to_file(file_path, chat_ids)
+    print("Updated chat IDs have been saved to the file.")
+
+def job():
+    collect_chat_ids()
+
+# Schedule the job to run every 20 seconds
+schedule.every(20).seconds.do(job)
 
 @app.route('/forward_notification', methods=['POST'])
 def forward_notification():
@@ -37,4 +57,11 @@ def forward_notification():
     return '', 200
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    # Run the Flask app in a separate thread
+    import threading
+    threading.Thread(target=app.run, kwargs={'port': 3000}).start()
+
+    # Run the scheduled job in the main thread
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
