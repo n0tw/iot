753e0cc7f6e4df6e9fae732d2417703291e393f0
n0tw <evgenia.123@hotmail.com>
2024-02-09 12:14:52 +0200

    map/busline_program/station_selection

--- backend.js
+++ backend.js
@@ -2,27 +2,76 @@ const express = require('express');
 const cors = require('cors');
 const socketIO = require('socket.io');
 const axios = require('axios');
+const bodyParser = require('body-parser');
 
 const app = express();
 const server = require('http').createServer(app);
-const io = socketIO(server);
-
-const buses = [
-  {
-      id: "urn:ngsi-ld:Vehicle:vehicle:WasteManagement:1",
-      name: "bus1"
-  },
-  {
-      id: "urn:ngsi-ld:Vehicle:vehicle:WasteManagement:2",
-      name: "bus2"
-  },
-  {
-      id: "urn:ngsi-ld:Vehicle:vehicle:WasteManagement:3",
-      name: "bus3"
-  }
-];
+const stations = [];
+const buses = [];
 let congestions = [];
 let busdata = [];
+const XLSX = require('xlsx');
+const fs = require('fs');
+
+const workbook = XLSX.readFile('Routes.xlsx');
+
+const sheetName = workbook.SheetNames[0];
+const worksheet = workbook.Sheets[sheetName];
+
+app.use(bodyParser.json());
+
+app.use(cors({
+    origin: 'http://127.0.0.1:3000',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+app.use(cors({
+    origin: 'http://127.0.0.1:3030',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+async function readexcel(){
+  const startRow = 1; // Example: Start parsing from the 2nd row
+  const endRow = 123;   // Example: Parse until the 5th row
+  const Cols = ['C','E','F','G','H','I','J','K'];
+
+  const parsedData = [];
+
+  for (let rowNum = startRow; rowNum <= endRow; rowNum++) {
+      const row = [];
+      let f = true;
+      for (let col of Cols) {
+          const cellAddress = { c: XLSX.utils.decode_col(col), r: rowNum };
+          const cellRef = XLSX.utils.encode_cell(cellAddress);
+          const cell = worksheet[cellRef];
+          if (cell && cell.w !== '-') {
+              row.push(cell.w);
+          } else {
+              f=false;
+              continue;
+          }
+      }
+      if (f){
+        parsedData.push(row);
+      }
+      
+  }
+  console.log(parsedData);
+  //document.getElementById("content_space")
+  return parsedData;
+}
+
+app.get('/getalltheroutes', async (req, res) => {
+  try {
+      const parsedData = await readexcel(req.query.id);
+      res.json(parsedData);
+  } catch (error) {
+      console.error('Error:', error.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 // Asynchronous function to read entity attribute
 const readEntityAttribute = async (entityId, attributeName) => {
   try {
@@ -40,116 +89,122 @@ const readEntityAttribute = async (entityId, attributeName) => {
       return null;
     }
   } catch (error) {
-    console.error('Error reading entity attribute:', error.message);
+    console.error(`Error reading entity attribute ${attributeName}:`, error.message);
     return null;
   }
 };
 
-// Asynchronous IIFE to execute async code before starting the server
-(async () => {
-  // Enable CORS for all routes
-  app.use(cors({
-    origin: 'http://127.0.0.1:3000', // Update this to match your frontend origin
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }));
+const readStationsLBinfo = async (entityId) => {
+  try {
+      const dT = await readEntityAttribute(entityId, 'dateObserved');
+      const l = await readEntityAttribute(entityId, "vehicleLastReported");
+      len =("urn:ngsild:Vehicle:Bus:").length;
+      cf = "urn:ngsild:CrowdFlowObserved:Bus:"+(l.value).slice(len, (l.value).length);
+      console.log("laaaaaaaaaaaaa", cf);
+      const lb = await readEntityAttribute(cf, "alternateName");
+
+      //console.log("name", await readEntityAttribute((await readEntityAttribute(entityId, "crowdFlowObserved")).value, 'name'));
+      return {
+          dateTime: dT,
+          lastbus: lb,
+      };
+      
+  }catch (error) {
+      console.error(`Error in readBusPCinfo ${entityId} :`, error.message);
+      return null;
+  }
+}
+
+app.get('/getStationsLB', async (req, res) => {
+  try {
+      const stationlasts = await readStationsLBinfo(req.query.id);
+      console.log(stationlasts);
+      res.json(stationlasts);
+  } catch (error) {
+      console.error('Error:', error.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/getBusDirection', async (req, res) => {
+  try {
+      const laststation = await readEntityAttribute(req.query.id, 'name');
+      console.log(laststation);
+      res.json(laststation);
+  } catch (error) {
+      console.error('Error:', error.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
   // Initiate the entity before starting the server
   /* await createEntities('39.556593793150746', '21.767370401805035', '38.24903100595789', '21.7393154225915'); */
 
-  // Read entity attribute
-  app.get('/getlocation', async (req, res) => {
-    const locationData = await readEntityAttribute('urn:ngsi-ld:Vehicle:vehicle:WasteManagement:1', 'location');
-    if (locationData && locationData.coordinates) {
-      const coordinates = locationData.coordinates;
-      console.log(`Coordinates:`, coordinates);
-      res.json({ 
-        location: coordinates,
-        time: time,
-        bus: bus
-      });
-    } else {
-      console.log(`Invalid location data for entity`);
-      res.status(500).json({ error: 'Invalid location data' });
-    }
-  });
+app.get('/getStationInfo', async (req, res) => {
+    console.log('GET /getStationInfo called');
+    res.json(stationData);
+});
 
-  app.get('/getStationInfo', async (req, res) => {
-    const stationId = req.query.stationid;
-    const locationData = await readEntityAttribute(stationId, 'location');
-    const time = await readEntityAttribute(stationId, 'dateLastReported');
-    const bus = await readEntityAttribute(stationId, 'vehicleLastReported');
-    if (locationData && locationData.coordinates) {
-      const coordinates = locationData.coordinates;
-      console.log(`Coordinates:`, coordinates);
-      res.json({ 
-        location: coordinates,
-        time: time,
-        bus: bus
-      });
-    } else {
-      console.log(`Invalid location data for entity`);
-      console.log("stationId", stationId);
-      res.status(500).json({ error: 'Invalid location data' });
-    }
-    console.log(`Attribute location value:`, locationData.coordinates);
-    console.log(`Attribute location value:`, time);
-    console.log(`Attribute location value:`, bus);
-    /* res.json({ 
-      location: loctn,
-      time: time,
-      bus: bus
-     }); */
+app.get('/getBusInfo', async (req, res) => {
+    res.json(busData);
+});
 
-  });
-  app.get('/getBusInfo', async (req, res) => {
-    res.json(busdata);
-  });
+app.get('/googlemaps', async (req, res) => {
+  try {
+    const { origin, destination, mode, key } = req.query;
+    const apiUrl = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&mode=${mode}&key=${key}`;
+    
+    const response = await axios.get(apiUrl);
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-  app.get('/googlemaps', async (req, res) => {
+
+const updateStationData = async () => {
     try {
-      const { origin, destination, key } = req.query;
-      const apiUrl = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${key}`;
-      
-      const response = await axios.get(apiUrl);
-      res.json(response.data);
+        stationData = await Promise.all(stations.map(async (station) => {
+            return {
+                id: station,
+                location: await readEntityAttribute(station, 'location'),
+                name: await readEntityAttribute(station, 'name')
+            };
+            
+        }));
     } catch (error) {
-      console.error('Error:', error.message);
-      res.status(500).json({ error: 'Internal Server Error' });
+        console.error('updateStationData Error updating station data:', error.message);
     }
-  });
-
-  const CongestedAllert = async (id) => {
-    console.log(`${id} congested!!`);
-  };
-
-  const CongestionStopped = async (id) => {
-    console.log(`Congestion of ${id} stopped.`);
-  };
+};
 
   const updateBusData = async () => {
     try {
-        busdata = await Promise.all(buses.map(async (bus) => {
-            const congestedid = await readEntityAttribute(bus.id, 'crowdFlowObserved');
-            const locationData = await readEntityAttribute(bus.id, 'location');
+        busData = await Promise.all(buses.map(async (bus) => {
+            //console.log("mnmnnmnmnmn");
+            const cf = await readEntityAttribute(bus, 'crowdFlowObserved');
+            const congested = await readEntityAttribute(cf.value, 'congested');
+            const locationData = await readEntityAttribute(bus, 'location');
+            if (!locationData) {
+                return;
+            }
+            
             return {
-                id: bus.id,
-                location: locationData.coordinates,
-                peopleCount: await readEntityAttribute(congestedid, 'peopleCount'),
-                congested: await readEntityAttribute(congestedid, 'congested'),
-                route: await readEntityAttribute(bus.id, 'serviceStatus')
+                id: bus,
+                location: locationData,
+                congested: congested.value,
+                crowdflowid: cf.value
             };
+                
         }));
 
           const filteredbuss = busdata.filter(bus => bus.congested === true && !congestions.includes(bus.id));
           const removebuss = busdata.filter(bus => bus.congested === false && congestions.includes(bus.id));
           congestions = congestions.filter(e => {
-              CongestionStopped(bus.id);
               !removebuss.map(bus => bus.id).includes(e);
           });
 
           filteredbuss.forEach(bus => {
-              CongestedAllert(bus.id);
               congestions.push(bus.id);
           });
 
@@ -158,27 +213,46 @@ const readEntityAttribute = async (entityId, attributeName) => {
       }
   };
 
+  const fData = async () => {
+    try {
+        // Populate the stations array
+        for (let i = 1; i < 33; i++) {
+            stations.push("urn:ngsild:TransportStation:Station:" + String(i));
+            //console.log("uuueueueueue");
+            console.log(await readEntityAttribute(stations[i - 1], 'crowdFlowObserved'));
+        }
 
-  updateBusData();
-  app.get('/getBusInfo', async (req, res) => {
-      res.json(busdata);
-  });
+        // Update station data
+        await updateStationData();
 
-  io.on('connection', (socket) => {
-    console.log('Client connected');
-    setInterval(() => {
-        updateBusData();
-        socket.emit('busupdate', busdata);
-    }, 3000);
+        // Update the /getStationInfo route with the updated station data
+        app.get('/getStationInfo', async (req, res) => {
+            console.log('GET /getStationInfo called');
+            res.json(stationData);
+        });
 
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');
-    });
-});
+        
+
+        for (let i = 1; i < 4; i++) {
+            buses.push("urn:ngsild:Vehicle:Bus:" + String(i));
+            console.log(await readEntityAttribute(buses[i-1], "crowdFlowObserved"));
+        }
+        await updateBusData();
+
+        app.get('/getBusInfo', async (req, res) => {
+            res.json(buses);
+        });
+
+    } catch (error) {
+        console.error('Error in fData:', error.message);
+    }
+};
+fData();
+console.log("kkkkkk",buses);
 
   // Start the Express server
   const PORT = 3000;
   server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
   });
-})();
+
